a63d9a6c427f0d93dbd47a70eac8be07370739e8
@jimanx2 <haziman.hashim@razer.com>
2023-02-04 17:16:50 +0800

    fix version

--- tests/Dummy/bootstrap/cache/packages.php
+++ tests/Dummy/bootstrap/cache/packages.php
@@ -6,13 +6,6 @@
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
   ),
-  'jimanx2/laravel-db-timezone' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Jimanx2\\LaravelDbTimezone\\Providers\\Provider',
-    ),
-  ),
   'laravel/tinker' => 
   array (
     'providers' => 
